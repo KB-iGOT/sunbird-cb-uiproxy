16850ccc1a36df11e2a24b45fa4c5839a760fc1d
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2024-10-15 21:10:30 +0530

    user event state read api (#122)

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -4708,6 +4708,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/user/event/state/read': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -5315,5 +5322,6 @@ export const API_LIST = {
             '/proxies/v8/event/batch/enroll',
             '/proxies/v8/event-progres/:eventId',
             '/proxies/v8/user/event/read/:id',
+            '/proxies/v8/user/event/state/read',
            ],
 }
