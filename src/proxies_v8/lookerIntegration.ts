14082c9e75ae88623e82e3a718a6e9b1a7d3615c
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-12-19 13:03:58 +0530

    Doing looker IntegrationImpl

--- src/proxies_v8/lookerIntegration.ts
+++ src/proxies_v8/lookerIntegration.ts
@@ -1,128 +1,130 @@
+import axios from 'axios'
 import crypto from 'crypto'
 import express from 'express'
-import querystring from 'querystring'
 import { CONSTANTS } from '../utils/env'
 import { logError, logInfo } from '../utils/logger'
 
 export const lookerDashboard = express.Router()
 
-interface ILookerOptions {
-  access_filters: Record<string, unknown>
-  embed_url: string
-  external_group_id: string
+interface IUser {
   external_user_id: string
   first_name: string
-  force_logout_login: boolean
-  group_ids: number[]
-  host: string
   last_name: string
-  models: string[]
   permissions: string[]
-  secret: string
-  session_length: number
+  models: string[]
+  access_filters: Record<string, unknown>
   user_attributes: Record<string, string>
+  group_ids: number[]
+  external_group_id: string
+  host?: string  // Added host to IUser interface if it's needed in the user object
+}
+
+class SignedCookie {
+  private looker: { host: string, secret: string }
+  private user: IUser
+  private sessionLength: number
+  private nonce = '' // Default value
+  private time = 0   // Default value
+  private signature = '' // Default value
+
+  constructor(looker: { host: string, secret: string }, user: IUser, sessionLength: number) {
+    this.looker = looker
+    this.user = user
+    this.sessionLength = sessionLength
+  }
+
+  generateCookie(): string {
+    this.setTime()
+    this.setNonce()
+    this.sign()
+
+    const payload = {
+      access_filters: JSON.stringify(this.user.access_filters),
+      external_group_id: this.user.external_group_id,
+      external_user_id: this.user.external_user_id,
+      group_ids: JSON.stringify(this.user.group_ids),
+      models: JSON.stringify(this.user.models),
+      nonce: this.nonce,
+      permissions: JSON.stringify(this.user.permissions),
+      sessionLength: this.sessionLength,
+      signature: this.signature,
+      time: this.time,
+      user_attributes: JSON.stringify(this.user.user_attributes),
+    }
+
+    return Buffer.from(JSON.stringify(payload)).toString('base64')
+  }
+
+  private toAscii(s: string): string {
+    return s.split('').map((ch) => String.fromCharCode(ch.charCodeAt(0))).join('')
+  }
+
+  private setTime() {
+    this.time = Math.floor(Date.now() / 1000)
+  }
+
+  private setNonce() {
+    this.nonce = this.toAscii(crypto.randomBytes(16).toString('base64'))
+  }
+
+  private sign() {
+    const stringToSign = [
+      this.looker.host,
+      this.nonce,
+      this.time,
+      this.sessionLength,
+      this.user.external_user_id,
+      JSON.stringify(this.user.permissions),
+      JSON.stringify(this.user.models),
+      JSON.stringify(this.user.group_ids),
+      JSON.stringify(this.user.external_group_id),
+      JSON.stringify(this.user.user_attributes),
+      JSON.stringify(this.user.access_filters),
+    ].join('\n')
+
+    const signer = crypto.createHmac('sha1', this.looker.secret)
+    this.signature = signer.update(stringToSign).digest('base64')
+  }
 }
 
 lookerDashboard.use('/*', async (req, res) => {
   const fifteenMinutes = 15 * 60
 
-  const lookerOptions: ILookerOptions = {
-    access_filters: { fake_model: { id: 1 } },
-    embed_url: req.query.embedUrl || '/embed/dashboards/9',
-    external_group_id: '5',
-    external_user_id: req.query.externalUserId || '31fa43e8-8123-43b9-997c-5c46d381ef7c',
-    first_name: req.query.firstName || 'Sahil',
-    force_logout_login: false,
-    group_ids: [4, 5],
+  const user: IUser = {
+  access_filters: { fake_model: { id: 1 } },
+  external_group_id: '5',
+  external_user_id: req.query.externalUserId || '31fa43e8-8123-43b9-997c-5c46d381ef7c',
+  first_name: req.query.firstName || 'Sahil',
+  group_ids: [4, 5],
+  host: CONSTANTS.LOOKER_HOST,
+  last_name: req.query.lastName || 'Chaudhary',
+  models: ['employee_enrolment', 'igot'],
+  permissions: ['see_user_dashboards', 'see_lookml_dashboards', 'access_data', 'see_looks'],
+  user_attributes: { example_attribute: 'attribute_value' },
+}
+
+  const lookerOptions = {
     host: CONSTANTS.LOOKER_HOST,
-    last_name: req.query.lastName || 'Chaudhary',
-    models: ['employee_enrolment', 'igot'],
-    permissions: ['see_user_dashboards', 'see_lookml_dashboards', 'access_data', 'see_looks'],
     secret: CONSTANTS.LOOKER_SECRET,
-    session_length: fifteenMinutes,
-    user_attributes: { example_attribute: 'attribute_value' },
   }
 
   try {
-    const signedUrl = createSignedEmbedUrl(lookerOptions)
+    const signedCookie = new SignedCookie(lookerOptions, user, fifteenMinutes).generateCookie()
 
-    logInfo(`Generated Looker dashboard URL: ${signedUrl}`)
-    res.status(200).json({ signedUrl })
+    logInfo(`Generated signed cookie for Looker embed` + signedCookie)
+    res.cookie('looker_auth', signedCookie, { httpOnly: true, secure: true })
+
+    const dashboardUrl = `https://${CONSTANTS.LOOKER_HOST}/dashboards/9`
+    const response = await axios.get(dashboardUrl, {
+      headers: {
+        Cookie: `looker_auth=${signedCookie}`,
+      },
+    })
+
+    logInfo(`Dashboard content retrieved successfully`)
+    res.status(200).json({ message: 'Cookie set and dashboard data retrieved successfully', data: response.data })
   } catch (err) {
-    logError('Error generating Looker dashboard URL:', err)
-    res.status(500).json({ error: 'Failed to generate Looker dashboard URL' })
+    logError('Error generating Looker signed cookie or retrieving dashboard data:', err)
+    res.status(500).json({ error: 'Failed to generate Looker signed cookie or retrieve dashboard data' })
   }
 })
-
-function createSignedEmbedUrl(options: ILookerOptions): string {
-  const {
-    secret,
-    host,
-    external_user_id,
-    first_name,
-    last_name,
-    group_ids,
-    external_group_id,
-    permissions,
-    models,
-    access_filters,
-    user_attributes,
-    session_length,
-    embed_url,
-    force_logout_login,
-  } = options
-
-  const nonce = () => {
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    return Array.from({ length: 16 }, () =>
-      possible.charAt(Math.floor(Math.random() * possible.length))
-    ).join('')
-  }
-
-  const forceUnicodeEncoding = (str: string) => decodeURIComponent(encodeURIComponent(str))
-
-  const embedPath = `/login/embed/${encodeURIComponent(embed_url)}`
-  const time = Math.floor(Date.now() / 1000)
-  const jsonNonce = nonce()
-
-  const stringToSign = [
-    host,
-    embedPath,
-    jsonNonce,
-    time,
-    session_length,
-    external_user_id,
-    JSON.stringify(permissions),
-    JSON.stringify(models),
-    JSON.stringify(group_ids),
-    JSON.stringify(external_group_id),
-    JSON.stringify(user_attributes),
-    JSON.stringify(access_filters),
-  ].join('\n')
-
-  const signature = crypto
-    .createHmac('sha1', secret)
-    .update(forceUnicodeEncoding(stringToSign))
-    .digest('base64')
-    .trim()
-
-  const queryParams = {
-    access_filters: JSON.stringify(access_filters),
-    embed_path: embedPath,
-    external_group_id,
-    external_user_id,
-    first_name,
-    force_logout_login,
-    group_ids: JSON.stringify(group_ids),
-    last_name,
-    models: JSON.stringify(models),
-    nonce: jsonNonce,
-    permissions: JSON.stringify(permissions),
-    session_length,
-    signature,
-    time,
-    user_attributes: JSON.stringify(user_attributes),
-  }
-
-  return `https://${host}${embedPath}?${querystring.stringify(queryParams)}`
-}
