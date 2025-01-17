8f16915bc4c2ab23edaf50bdf510edccd6418ebb
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-01-17 11:02:50 +0530

    4.8.21 dev v2 (#202)
    * Added ui-proxy for community category CRUD and list API (#193)
    
    * Added ui-proxy for sub-category list API (#195)
    
    * Cbrelease 4.8.21 (#197)
    
    * 4.8.20 dev v4 (#183)
    
    * added report api for ui-proxy (#175)
    
    * added uploadFile api (#176)
    
    ---------
    
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    
    * upload api bug fix (#184)
    
    * 4.8.20 looker imp v2 (#185)
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Revert to signedUrlImpl
    
    * Adding the logging
    
    * Adding the logging
    
    * Adding the logging
    
    * Update the stringfy
    
    * Update the stringfy
    
    * Update the stringfy
    
    * update nonce generation method
    
    * update the api looker version2
    
    * update the api looker version2
    
    * update the api looker version2
    
    * update the api looker version2
    
    * Update the integration Impl
    
    * Updating private user update API endpoint (#188)
    
    * Cbrelease 4.8.19 merge (#191)
    
    * Adding the looker Integration Impl (#186)
    
    * making the API changes for looker Integration (#190)
    
    * Added v2 version of workflow transition API (#194) (#196)
    
    Co-authored-by: vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>
    Co-authored-by: vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
    
    * progress api added (#199)
    
    * Cbrelease 4.8.21 (#201)
    
    * 4.8.20 dev v4 (#183)
    
    * added report api for ui-proxy (#175)
    
    * added uploadFile api (#176)
    
    ---------
    
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    
    * upload api bug fix (#184)
    
    * 4.8.20 looker imp v2 (#185)
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Doing looker IntegrationImpl
    
    * Revert to signedUrlImpl
    
    * Adding the logging
    
    * Adding the logging
    
    * Adding the logging
    
    * Update the stringfy
    
    * Update the stringfy
    
    * Update the stringfy
    
    * update nonce generation method
    
    * update the api looker version2
    
    * update the api looker version2
    
    * update the api looker version2
    
    * update the api looker version2
    
    * Update the integration Impl
    
    * Updating private user update API endpoint (#188)
    
    * Cbrelease 4.8.19 merge (#191)
    
    * Adding the looker Integration Impl (#186)
    
    * making the API changes for looker Integration (#190)
    
    * Added v2 version of workflow transition API (#194) (#196)
    
    Co-authored-by: vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>
    Co-authored-by: vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
    
    * Adding the API changes for bulk upload designation
    
    * Adding the API changes for bulk upload designation
    
    ---------
    
    Co-authored-by: ruksana2808 <111083125+ruksana2808@users.noreply.github.com>
    Co-authored-by: SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>
    Co-authored-by: vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
    Co-authored-by: Arpitha <arpitha.sureshappa@tarento.com>

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -4554,7 +4554,7 @@ export const API_LIST = {
                 ROLE.SPV_ADMIN,
             ],
         },
-        '/proxies/v8/designation/v1/orgMapping/bulkUpload/:frameworkId': {
+        '/proxies/v8/designation/v1/orgMapping/bulkUpload/:orgId/:frameworkId': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
@@ -5056,6 +5056,60 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.PUBLIC,
             ],
+        },
+        '/proxies/v8/community/v1/category/create': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                     ROLE.SPV_ADMIN,
+                      ROLE.MDO_ADMIN,
+                  ],
+        },
+        '/proxies/v8/community/v1/category/update': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                     ROLE.SPV_ADMIN,
+                      ROLE.MDO_ADMIN,
+                  ],
+        },
+        '/proxies/v8/community/v1/category/read/:categoryId': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                    ROLE.PUBLIC,
+                  ],
+        },
+        '/proxies/v8/community/v1/category/delete/:categoryId': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                     ROLE.SPV_ADMIN,
+                      ROLE.MDO_ADMIN,
+                  ],
+        },
+        '/proxies/v8/community/v1/category/list': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                    ROLE.PUBLIC,
+                  ],
+        },
+        '/proxies/v8/community/v1/subcategory/list': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                    ROLE.PUBLIC,
+                  ],
+        },
+        '/proxies/v8/serviceregistry/v1/callexternal/progressapibyid/:id': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                    ROLE.SPV_ADMIN,
+                    ROLE.CBP_ADMIN,
+                    ROLE.SPV_PUBLISHER,
+                  ],
         },
     },
     URL_PATTERN:
@@ -5643,7 +5697,7 @@ export const API_LIST = {
             '/proxies/v8/organisation/v1/competencyDesignationMappings/download/:fileName',
             '/proxies/v8/national/learning/week/insights',
             '/proxies/v8/designation/v1/orgMapping/sample/:frameworkId',
-            '/proxies/v8/designation/v1/orgMapping/bulkUpload/:frameworkId',
+            '/proxies/v8/designation/v1/orgMapping/bulkUpload/:orgId/:frameworkId',
             '/proxies/v8/designation/v1/orgMapping/bulkUpload/progress/details/:orgId',
             '/proxies/v8/designation/v1/orgMapping/download/:fileName',
             '/proxies/v8/ciosIntegration/v1/read/content/:partnercode/:externalid',
@@ -5708,5 +5762,12 @@ export const API_LIST = {
             '/proxies/v8/learner/course/v4/user/enrollment/summary/:id',
             '/proxies/v8/learner/course/v4/user/enrollment/list/:id',
             '/proxies/v8/learner/course/v4/user/enrollment/details/:id',
+            '/proxies/v8/community/v1/category/create',
+            '/proxies/v8/community/v1/category/update',
+            '/proxies/v8/community/v1/category/read/:categoryId',
+            '/proxies/v8/community/v1/category/delete/:categoryId',
+            '/proxies/v8/community/v1/category/list',
+            '/proxies/v8/community/v1/subcategory/list',
+            '/proxies/v8/serviceregistry/v1/callexternal/progressapibyid/:id',
            ],
 }
