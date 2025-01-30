0bd41eec04db3af5acc85b6d80321c73e3682fc8
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-01-30 17:17:55 +0530

    Adding the same site and do formatting

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -12,17 +12,17 @@ userAuthKeyCloakApi.get('/', (req, res) => {
     if (req.session && req.session.authenticated ) {
         logInfo('User is authenticated.. Updating Cookie with Secure and SameSite flags')
         if (host !== undefined) {
-          if (host.includes('localhost')) {
-                      domain = 'localhost' // For localhost, set domain to localhost
-                  } else {
-                      const hostParts = host.split('.')
-                      if (hostParts.length > 2) {
-                          domain = '.' + hostParts.slice(1).join('.')
-                      } else {
-                          domain = host
-                      }
-                    }
-          }
+            if (host.includes('localhost')) {
+                domain = 'localhost' // For localhost, set domain to localhost
+            } else {
+                const hostParts = host.split('.')
+                if (hostParts.length > 2) {
+                    domain = '.' + hostParts.slice(1).join('.')
+                } else {
+                    domain = host
+                }
+            }
+        }
         res.cookie('connect.sid', req.cookies['connect.sid'], {
             domain,
             httpOnly: true,
--- src/server.ts
+++ src/server.ts
@@ -190,18 +190,18 @@ export class Server {
       const host = _req.get('host')
       let domain = ''
       if (host !== undefined) {
-          if (host.includes('localhost')) {
-                      domain = 'localhost' // For localhost, set domain to localhost
-                  } else {
-                      const hostParts = host.split('.')
-                      if (hostParts.length > 2) {
-                          domain = '.' + hostParts.slice(1).join('.')
-                      } else {
-                          domain = host
-                      }
-                    }
+        if (host.includes('localhost')) {
+          domain = 'localhost' // For localhost, set domain to localhost
+        } else {
+          const hostParts = host.split('.')
+          if (hostParts.length > 2) {
+            domain = '.' + hostParts.slice(1).join('.')
+          } else {
+            domain = host
           }
-      res.clearCookie('connect.sid', { domain, httpOnly: false, path: '/', secure: true })
+        }
+      }
+      res.clearCookie('connect.sid', { domain, httpOnly: false, path: '/', secure: true, sameSite: 'None' })
       if (_req.session) {
         _req.session.destroy(() => {
           logInfo('Session Destroyed')
