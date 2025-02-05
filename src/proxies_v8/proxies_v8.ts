227251f8dd4639a6ccbacd0326bc71973e9e04af
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-02-05 11:31:40 +0530

    added course recommendation api's (#215)

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -1140,3 +1140,11 @@ proxiesV8.use('/courseRecommend/v1/courses',
   // tslint:disable-next-line: max-line-length
   proxyCreatorSunbirdSearch(express.Router(), `${CONSTANTS.KONG_API_BASE}/courseRecommend/v1/courses`)
 )
+
+proxiesV8.use('/courseRecommendation/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
+
+proxiesV8.use('/chatbot/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
