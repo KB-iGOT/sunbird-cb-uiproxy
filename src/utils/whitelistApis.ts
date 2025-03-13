3d633358b901395ca20e8b5222d40b2a2efbff44
ruksana2808 <111083125+ruksana2808@users.noreply.github.com>
2025-03-13 17:28:22 +0530

    Added ui-proxy for communityPublish API (#252)

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -5378,6 +5378,15 @@ export const API_LIST = {
                     ROLE.PUBLIC,
                   ],
         },
+        '/proxies/v8/community/v1/publish': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                     ROLE.SPV_ADMIN,
+                     ROLE.MDO_ADMIN,
+                     ROLE.MDO_LEADER,
+                  ],
+        },
     },
     URL_PATTERN:
         [
@@ -6071,6 +6080,7 @@ export const API_LIST = {
             '/proxies/v8/feedDiscussion/answerPost/like/:id',
             '/proxies/v8/feedDiscussion/answerPost/delete/:id',
             '/proxies/v8/feedDiscussion/answerPost/dislike/:id',
-            '/proxies/v8/thumbnail/v1/image/variations/course/:id'
+            '/proxies/v8/thumbnail/v1/image/variations/course/:id',
+            '/proxies/v8/community/v1/publish',
            ],
 }
