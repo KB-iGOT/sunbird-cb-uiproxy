199a3df41d0b377034ad6ebce45ed773a816d8d9
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-04-12 17:21:55 +0530

    Adding the chatbot search API (#272)

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -5422,6 +5422,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/chatbot/v3/search': {
+            checksNeeded: [CHECK.ROLE],
+             // tslint:disable-next-line: object-literal-sort-keys
+             ROLE_CHECK: [
+                ROLE.PUBLIC,
+             ],
+         },
     },
     URL_PATTERN:
         [
@@ -6122,5 +6129,6 @@ export const API_LIST = {
             '/proxies/v8/feedDiscussion/v1/enrichData',
             '/proxies/v8/user/v5/public/search',
             '/proxies/v8/community/v1/user/communities/all',
+            '/proxies/v8/chatbot/v3/search',
            ],
 }
