49868efb540246f0c4030556c7dbbbdbe0a2bff2
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-05-29 16:25:02 +0530

    4.8.26 dev v2 (#322)
    * Sprint 26 dev (#317)
    
    * new 2 API from workflow service where we are downloading csv file and one more process csv file for approval. (#304)
    
    * added user/admin/search and org/admin/search
    
    ---------
    
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    
    * KB-9806 |  BE| DEV | UI proxy ,Kong/onboard APIS for ext service. (#318)
    
    1. Added ui proxy for location details.
    
    * added new role for the reported Post management api's (#321)
    
    * Update roles.ts COMMUNITY_MODERATOR: 'COMMUNITY_MODERATOR', updated
    
    ---------
    
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    Co-authored-by: DeepikaMalav28 <deepika.malav@tarento.com>
    Co-authored-by: Rajeev Sathish <rajeev.sathish@tarento.com>
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    Co-authored-by: Haritest <haridasksd@gmail.com>

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -497,6 +497,7 @@ export const API_LIST = {
                 ROLE.SPV_ADMIN,
                 ROLE.STATE_ADMIN,
                 ROLE.MDO_LEADER,
+                ROLE.COMMUNITY_MODERATOR,
             ],
         },
         '/proxies/v8/data/v1/system/settings/get/notificationPreference': {
@@ -511,6 +512,21 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.PUBLIC,
+            ],
+        },
+        '/proxies/v8/org/v1/admin/search': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+               ROLE.MDO_ADMIN,
+               ROLE.MDO_LEADER,
+               ROLE.SPV_ADMIN,
+               ROLE.CONTENT_PUBLISHER,
+               ROLE.CONTENT_CREATOR,
+               ROLE.CONTENT_REVIEWER,
+               ROLE.SPV_PUBLISHER,
+               ROLE.CBP_ADMIN,
+               ROLE.PROGRAM_COORDINATOR,
             ],
         },
         '/proxies/v8/user/private/v1/assign/role': {
@@ -530,6 +546,21 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.PUBLIC,
+            ],
+        },
+        '/proxies/v8/user/v1/admin/search': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+               ROLE.MDO_ADMIN,
+               ROLE.MDO_LEADER,
+               ROLE.SPV_ADMIN,
+               ROLE.CONTENT_PUBLISHER,
+               ROLE.CONTENT_CREATOR,
+               ROLE.CONTENT_REVIEWER,
+               ROLE.SPV_PUBLISHER,
+               ROLE.CBP_ADMIN,
+               ROLE.PROGRAM_COORDINATOR,
             ],
         },
         '/proxies/v8/org/v1/read': {
@@ -4988,6 +5019,7 @@ export const API_LIST = {
                ROLE.PUBLIC,
                ROLE.MDO_LEADER,
                ROLE.MDO_ADMIN,
+               ROLE.COMMUNITY_MODERATOR,
             ],
         },
         '/proxies/v8/feedDiscussion/question/like/:id': {
@@ -5763,6 +5795,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                ROLE.MDO_ADMIN,
                ROLE.MDO_LEADER,
+               ROLE.COMMUNITY_MODERATOR,
             ],
         },
         '/proxies/v8/feedDiscussion/admin/removePost': {
@@ -5771,6 +5804,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                ROLE.MDO_ADMIN,
                ROLE.MDO_LEADER,
+               ROLE.COMMUNITY_MODERATOR,
             ],
         },
         '/proxies/v8/feedDiscussion/getReportStatistics': {
@@ -5779,6 +5813,7 @@ export const API_LIST = {
             ROLE_CHECK: [
                ROLE.MDO_ADMIN,
                ROLE.MDO_LEADER,
+               ROLE.COMMUNITY_MODERATOR,
             ],
         },
        '/proxies/v8/workflow/blendedprogram/getUserApprovalDataInCsv': {
@@ -5944,6 +5979,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/user/profile/v1/extended/locationDetails/:id': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+               ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -5971,6 +6013,7 @@ export const API_LIST = {
             '/proxies/v8/data/v1/system/settings/get/orgTypeList',
             '/proxies/v8/data/v1/system/settings/get/notificationPreference',
             '/proxies/v8/org/v1/search',
+            '/proxies/v8/org/v1/admin/search',
             '/proxies/v8/org/v1/update',
             '/proxies/v8/notifyContentState',
             '/proxies/v8/discussion/topic/:id/:slug',
@@ -6006,6 +6049,7 @@ export const API_LIST = {
             '/proxies/v8/action/content/v3/reject/:do_id',
             '/proxies/v8/user/private/v1/assign/role',
             '/proxies/v8/user/v1/search',
+            `/proxies/v8/user/v1/admin/search`,
             '/proxies/v8/org/v1/read',
             '/proxies/v8/org/v1/profile/read',
             '/proxies/v8/org/v1/profile/patch',
@@ -6705,6 +6749,7 @@ export const API_LIST = {
             '/proxies/v8/v1/notifications/bulk/create',
             '/proxies/v8/v1/notifications/unread/count',
             '/proxies/v8/masterdata/update/degree',
-            '/proxies/v8/masterdata/update/institution'
+            '/proxies/v8/masterdata/update/institution',
+            '/proxies/v8/user/profile/v1/extended/locationDetails/:id'
             ],
 }
