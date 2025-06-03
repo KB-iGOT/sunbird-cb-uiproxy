a6d19834d779ae0f220e38818f0607b57fa9e8fc
Ruksana Semeir <111083125+ruksana2808@users.noreply.github.com>
2025-06-03 17:42:07 +0530

    Added ui-proxy for accessRule Read API

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6006,6 +6006,16 @@ export const API_LIST = {
                 ROLE.SPV_PUBLISHER,
             ],
         },
+        '/proxies/v8/accessSetttings/v1/read/:contentId': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.CONTENT_CREATOR,
+                ROLE.CONTENT_PUBLISHER,
+                ROLE.CONTENT_REVIEWER,
+                ROLE.SPV_PUBLISHER,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -6773,5 +6783,6 @@ export const API_LIST = {
             '/proxies/v8/user/profile/v1/extended/locationDetails/:id',
             '/proxies/v8/accessSetttings/v1/upsert',
             '/proxies/v8/user/v1/groups',
+            '/proxies/v8/accessSetttings/v1/read/:contentId',
             ],
 }
