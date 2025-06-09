aa9f976d1d3b1545ef6c3d747be38e55fa4bd7d8
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-06-09 13:36:00 +0530

    adding the gloabal search chat Bot API

---
+++ src/proxies_v8/chatBotGenericAPIIntegration.ts
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import express from 'express'
+import { axiosRequestConfig } from '../configs/request.config'
+import { CONSTANTS } from '../utils/env'
+import { logError, logInfo } from '../utils/logger'
+
+export const chatBotGenericAPIIntegration = express.Router()
+
+chatBotGenericAPIIntegration.use('/*', async (req: express.Request, res: express.Response) => {
+    try {
+
+        const baseUrl = removePrefix('/proxies/v8/chatbot/v3/global', req.originalUrl)
+        logInfo(`The url is... ${baseUrl} : originalUrl: ${req.originalUrl}`)
+        const subPath = baseUrl.replace(/^\/+/, '')
+        const url = `${CONSTANTS.APP_FUEL_GLOBAL_API_URL}/${subPath}`
+        const requestBody = req.body
+        logInfo(`Chatbot API Request -> URL: ${url}`)
+        const axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            ...axiosRequestConfig,
+        }
+
+        let response
+        if (req.method === 'GET') {
+            response = await axios.get(url, axiosConfig)
+        } else if (req.method === 'POST') {
+            response = await axios.post(url, requestBody, axiosConfig)
+        } else if (req.method === 'PUT') {
+            response = await axios.put(url, requestBody, axiosConfig)
+        } else if (req.method === 'DELETE') {
+            response = await axios.delete(url, axiosConfig)
+        } else {
+            return res.status(405).send({ error: `Method ${req.method} not supported` })
+        }
+        return res.status(response.status).send(response.data)
+    } catch (error) {
+        logError(`Error in chatBotGenericAPIIntegration`, error)
+        return res.status(500).send({ error: 'Failed to fetch data from global chatbot API' })
+    }
+})
+
+function removePrefix(prefix: string, s: string): string {
+  return s.startsWith(prefix) ? s.substring(prefix.length) : s
+}
--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6054,6 +6054,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
              ],
          },
+        '/proxies/v8/chatbot/v3/global/search': {
+            checksNeeded: [CHECK.ROLE],
+             // tslint:disable-next-line: object-literal-sort-keys
+             ROLE_CHECK: [
+                ROLE.PUBLIC,
+             ],
+         },       
     },
     URL_PATTERN:
         [
@@ -6827,5 +6834,6 @@ export const API_LIST = {
             '/proxies/v8/accessSetttings/v1/delete/:contentId',
             '/proxies/v8/search/v1/recent/delete/timestamp/:timestamp',
             '/proxies/v8/chatbot/v3/transcoder/stats',
+            '/proxies/v8/chatbot/v3/global/search'
             ],
 }
