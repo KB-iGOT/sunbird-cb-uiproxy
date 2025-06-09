32e605d99bbe798ef66b88e7d8ee9eec294492cb
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-06-09 15:33:19 +0530

    added org hierarchy framework api

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6047,6 +6047,16 @@ export const API_LIST = {
                  ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/org/framework/v1/create': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+             ROLE.MDO_ADMIN,
+             ROLE.MDO_LEADER,
+             ROLE.SPV_ADMIN,
+             ROLE.SPV_PUBLISHER,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -6819,5 +6829,6 @@ export const API_LIST = {
             '/proxies/v8/accessSetttings/read/:contentId',
             '/proxies/v8/accessSetttings/v1/delete/:contentId',
             '/proxies/v8/search/v1/recent/delete/timestamp/:timestamp',
+            '/proxies/v8/org/framework/v1/create',
             ],
 }
