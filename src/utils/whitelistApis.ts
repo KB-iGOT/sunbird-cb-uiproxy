8b683efb7fa0ae8e44fe7168c942f657c85c5220
DeepikaMalav28 <deepika.malav@tarento.com>
2025-06-14 00:19:37 +0530

    add unresetCount api

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -5960,6 +5960,13 @@ export const API_LIST = {
                  ROLE.PUBLIC,
              ],
         },
+        '/proxies/v8/v1/notifications/reset/unread/count': {
+                     checksNeeded: [CHECK.ROLE],
+                     // tslint:disable-next-line: object-literal-sort-keys
+                     ROLE_CHECK: [
+                         ROLE.PUBLIC,
+                     ],
+                },
        '/proxies/v8/masterdata/update/degree': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
@@ -6953,6 +6960,7 @@ export const API_LIST = {
             '/proxies/v8/v1/notifications/delete',
             '/proxies/v8/v1/notifications/bulk/create',
             '/proxies/v8/v1/notifications/unread/count',
+            '/proxies/v8/v1/notifications/reset/unread/count'
             '/proxies/v8/masterdata/update/degree',
             '/proxies/v8/masterdata/update/institution',
             '/proxies/v8/user/profile/v1/extended/locationDetails/:id'
