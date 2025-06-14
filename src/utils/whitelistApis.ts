d45f8dffa21a3a22cd5efc0239c975b9ed65ee25
DeepikaMalav28 <deepika.malav@tarento.com>
2025-06-14 14:52:56 +0530

    reset count (#353)
    * Sprint 26 dev (#317)
    
    * new 2 API from workflow service where we are downloading csv file and one more process csv file for approval. (#304)
    
    * added user/admin/search and org/admin/search
    
    ---------
    
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    
    * KB-9806 |  BE| DEV | UI proxy ,Kong/onboard APIS for ext service. (#318)
    
    1. Added ui proxy for location details.
    
    * add unresetCount api
    
    * remove
    
    * comma add
    
    * fix
    
    ---------
    
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -5960,6 +5960,13 @@ export const API_LIST = {
                  ROLE.PUBLIC,
              ],
         },
+        '/proxies/v8/v1/notifications/reset/unread/count': {
+                     checksNeeded: [CHECK.ROLE],
+                     // tslint:disable-next-line: object-literal-sort-keys
+                     ROLE_CHECK: [
+                         ROLE.PUBLIC,
+                     ],
+         },          
        '/proxies/v8/masterdata/update/degree': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
@@ -5981,6 +5988,7 @@ export const API_LIST = {
                ROLE.PUBLIC,
             ],
         },
+
         '/proxies/v8/chatbot/v3/feedbacks/save': {
             checksNeeded: [CHECK.ROLE],
              // tslint:disable-next-line: object-literal-sort-keys
@@ -6951,6 +6959,7 @@ export const API_LIST = {
             '/proxies/v8/v1/notifications/delete',
             '/proxies/v8/v1/notifications/bulk/create',
             '/proxies/v8/v1/notifications/unread/count',
+            '/proxies/v8/v1/notifications/reset/unread/count',
             '/proxies/v8/masterdata/update/degree',
             '/proxies/v8/masterdata/update/institution',
             '/proxies/v8/user/profile/v1/extended/locationDetails/:id',
@@ -6979,5 +6988,6 @@ export const API_LIST = {
             '/proxies/v8/connections/v1/profile/relationship/:userId',
             '/proxies/v8/customFields/v1/masterList/update',
             '/proxies/v8/customFields/v1/status/update',
+
             ],
 }
