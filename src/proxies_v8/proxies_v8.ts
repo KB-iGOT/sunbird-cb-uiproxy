753c25beaecf22eb381c877fcb188268d85d8292
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-06-17 16:41:13 +0530

    4.8.26 dev v6 (#357)
    * added status update and masterList update apis (#347)
    
    * added confg. for org_hierarchy bulk upload (#348)
    
    * added the update api to file upload api list
    
    * bug fix for the update api (#350)
    
    * bug fix for the masterList api (#351)
    
    * 4.8.26 fix (#352)
    
    * updating the code for text/csv
    
    * updating the code for text/csv
    
    * updating the code for text/csv
    
    * updating the code for text/csv
    
    * reset count (#353)
    
    * Sprint 26 dev (#317)
    
    * new 2 API from workflow service where we are downloading csv file and one more process csv file for approval. (#304)
    
    * added user/admin/search and org/admin/search
    
    ---------
    
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    
    * KB-9806 |  BE| DEV | UI proxy ,Kong/onboard APIS for ext service. (#318)
    
    1. Added ui proxy for location details.
    
    * add unresetCount api
    
    * remove
    
    * comma add
    
    * fix
    
    ---------
    
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    
    * Sprint 26 dev (#356)
    
    * added user/admin/search and org/admin/search
    
    * Added ui-proxy for accessRule upsert API (#323)
    
    * added /proxies/v8/user/v1/groups to whiteListing
    
    * added comma at the end
    
    * added new api user/profile/v1/extended/competencies/:userId
    
    * added new api /connections/* and whiteList /proxies/v8/connections/v1/profile/relationship/:userId
    
    * added proxiy api for update category framework
    
    ---------
    
    Co-authored-by: Ruksana Semeir <111083125+ruksana2808@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
    Co-authored-by: anilkumarkammalapalli <121931293+anilkumarkammalapalli@users.noreply.github.com>
    Co-authored-by: sureshece16 <80331286+sureshece16@users.noreply.github.com>
    Co-authored-by: DeepikaMalav28 <deepika.malav@tarento.com>
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    Co-authored-by: Ruksana Semeir <111083125+ruksana2808@users.noreply.github.com>

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -439,7 +439,7 @@ proxiesV8.use('/dashboard/*',
 )
 
 // tslint:disable-next-line:max-line-length
-proxiesV8.post(['/user/v1/bulkupload', '/storage/profilePhotoUpload/*', '/workflow/admin/transition/bulkupdate', '/cloud-services/mlcore/v1/files/upload', '/calendar/v1/bulkUpload', '/storage/orgStoreUpload', '/workflow/admin/v2/bulkupdate/transition', '/user/v2/bulkupload', '/ciosIntegration/v1/loadContentFromExcel/*', '/storage/v1/uploadCiosIcon', '/storage/v1/uploadCiosContract', '/organisation/v1/competencyDesignationMappings/bulkUpload/*', '/template/api/v1/upload', '/designation/v1/orgMapping/bulkUpload/*', '/storage/v1/uploadCiosLogsFile', '/customselfregistration/upload/logo/gcpcontainer', '/ciosIntegration/v1/loadContentProgressFromExcel/*', '/feedDiscussion/uploadFile/*', '/community/v1/fileUpload/*', '/user/v2/event/bulkonboard/*', '/workflow/blendedprogram/bulkApprovalDataFromCsv/*', '/customFields/v1/masterList/create', '/organisation/v1/hierarchy/bulkUpload/*', '/customFields/v1/masterList/update'], (req, res) => {
+proxiesV8.post(['/user/v1/bulkupload', '/storage/profilePhotoUpload/*', '/workflow/admin/transition/bulkupdate', '/cloud-services/mlcore/v1/files/upload', '/calendar/v1/bulkUpload', '/storage/orgStoreUpload', '/workflow/admin/v2/bulkupdate/transition', '/user/v2/bulkupload', '/ciosIntegration/v1/loadContentFromExcel/*', '/storage/v1/uploadCiosIcon', '/storage/v1/uploadCiosContract', '/organisation/v1/competencyDesignationMappings/bulkUpload/*', '/template/api/v1/upload', '/designation/v1/orgMapping/bulkUpload/*', '/storage/v1/uploadCiosLogsFile', '/customselfregistration/upload/logo/gcpcontainer', '/ciosIntegration/v1/loadContentProgressFromExcel/*', '/feedDiscussion/uploadFile/*', '/community/v1/fileUpload/*', '/user/v2/event/bulkonboard/*', '/workflow/blendedprogram/bulkApprovalDataFromCsv/*', '/customFields/v1/masterList/*', '/organisation/v1/hierarchy/bulkUpload/*'], (req, res) => {
   if (req.files && req.files.data) {
     const url = removePrefix('/proxies/v8', req.originalUrl)
     const file: UploadedFile = req.files.data as UploadedFile
@@ -481,17 +481,28 @@ proxiesV8.post(['/user/v1/bulkupload', '/storage/profilePhotoUpload/*', '/workfl
       // tslint:disable-next-line: all
       (err, response) => {
         // tslint:disable-next-line: all
-        response.on('data', (data) => {
+        let chunks: Buffer[] = [];
+        response.on('data', (chunk) => chunks.push(chunk))
+        // tslint:disable-next-line: all
+        response.on('end', () => {
+          const fullData = Buffer.concat(chunks)
           if (!err && (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 406)) {
             if (response.headers['content-type'] === 'text/csv') {
               res.setHeader('Content-Type', 'text/csv')
               res.setHeader('Content-Disposition', 'attachment; filename="report.csv"')
-              res.status(response.statusCode).send(data)
+              res.status(response.statusCode).send(fullData)
             } else {
-              res.status(response.statusCode).send(JSON.parse(data.toString('utf8')))
+              let parsed
+              try {
+                  parsed = JSON.parse(fullData.toString('utf8'))
+                  res.status(response.statusCode).json(parsed)
+              } catch (e) {
+                  logInfo('Invalid JSON received as per Json Parse')
+                  res.status(response.statusCode).type('application/json').send(fullData.toString('utf8'))
+              }
             }
           } else {
-            res.status(500).send(data.toString('utf8'))
+            res.status(500).send(fullData.toString('utf8'))
           }
         })
         if (err) {
@@ -540,17 +551,28 @@ proxiesV8.post(['/user/v1/bulkupload', '/storage/profilePhotoUpload/*', '/workfl
       // tslint:disable-next-line: all
       (err, response) => {
         // tslint:disable-next-line: all
-        response.on('data', (data) => {
+        let chunks: Buffer[] = [];
+        response.on('data', (chunk) => chunks.push(chunk))
+        // tslint:disable-next-line: all
+        response.on('end', () => {
+          const fullData = Buffer.concat(chunks)
           if (!err && (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 406)) {
             if (response.headers['content-type'] === 'text/csv') {
               res.setHeader('Content-Type', 'text/csv')
               res.setHeader('Content-Disposition', 'attachment; filename="report.csv"')
-              res.status(response.statusCode).send(data)
+              res.status(response.statusCode).send(fullData)
             } else {
-              res.status(response.statusCode).send(JSON.parse(data.toString('utf8')))
+              let parsed
+              try {
+                  parsed = JSON.parse(fullData.toString('utf8'))
+                  res.status(response.statusCode).json(parsed)
+              } catch (e) {
+                   logInfo('Invalid JSON received as per Json Parse')
+                   res.status(response.statusCode).type('application/json').send(fullData.toString('utf8'))
+              }
             }
           } else {
-            res.status(500).send(data.toString('utf8'))
+            res.status(500).send(fullData.toString('utf8'))
           }
         })
         if (err) {
