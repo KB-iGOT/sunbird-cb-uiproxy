b3e9292a3c1d54f5c71a20b42e0da7c4ad8f0ee8
DeepikaMalav28 <deepika.malav@tarento.com>
2025-06-23 17:48:16 +0530

    proxies add (#379)

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -1311,6 +1311,10 @@ proxiesV8.use('/v1/notifications/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
+proxiesV8.use('/notificationSetting/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
+
 proxiesV8.use('/accessSetttings*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
