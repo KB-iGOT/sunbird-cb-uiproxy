9191c40787459a31c4e6fd488274f2c7275b80b2
Ruksana Semeir <111083125+ruksana2808@users.noreply.github.com>
2025-06-26 15:46:23 +0530

    Added ui-proxy for contentStateV2Update

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6275,6 +6275,17 @@ export const API_LIST = {
                ROLE.MDO_LEADER,
             ],
         },
+        '/proxies/v8/content/v2/state/update': {
+            checksNeeded: [CHECK.ROLE],
+             // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+               ROLE.MDO_ADMIN,
+               ROLE.MDO_LEADER,
+               ROLE.SPV_ADMIN,
+               ROLE.CONTENT_CREATOR,
+               ROLE.SPV_PUBLISHER,
+            ],
+         },
     },
     URL_PATTERN:
         [
@@ -7075,5 +7086,6 @@ export const API_LIST = {
             '/proxies/v8/support/ai/chat/start',
             '/proxies/v8/support/ai/chat/send',
             '/proxies/v8/customFields/v1/popup/update',
+            '/proxies/v8/content/v2/state/update',
             ],
 }
