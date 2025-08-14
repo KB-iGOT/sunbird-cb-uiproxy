fc4d6f302d1096918bd359703a0214d7d197946f
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2025-08-14 17:29:49 +0530

    Update language detection API in proxies_v8.ts

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -1338,7 +1338,7 @@ proxiesV8.use('/collection/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
 
-proxiesV8.use('/content/v1/language/detect',
+proxiesV8.use('/moderation/v1/language/detect',
   // tslint:disable-next-line: max-line-length
-  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}/v1/content/moderation/detect-language`)
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}/v1/content/language/detect`)
 )
