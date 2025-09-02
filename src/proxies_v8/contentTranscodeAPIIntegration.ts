534e507901646b6c03a190a23efc66653b62d1b5
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-09-02 10:56:20 +0530

    Cbrelease 4.8.28 (#420) (#421)
    * adding the review ML Course API
    
    * adding the update review status ML Course API
    
    * 4.8.27.2 dev v6 (#406)
    
    * KB-10796 added request query params. (#405)
    
    * added language here which is comeing from request header
    
    * KB-10796 added request query params.
    
    ---------
    
    
    
    * update the autoEnrollment API fix
    
    ---------
    
    
    
    
    * adding the hyper4 content transcode API
    
    * Cbrelease 4.8.27.2 (#417)
    
    * KB-11384| BE| CbPlan create API with accessSettings (#414)
    
    * Added ui-proxy for cbPlanV2Update API (#415)
    
    * KB-11384| BE| CbPlan create , read , publishand update API with accessSettings (#416)
    
    ---------
    
    
    
    * adding the hyper4 content transcode API (#419)
    
    # Conflicts:
    #	src/proxies_v8/proxies_v8.ts
    
    ---------
    
    Co-authored-by: Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
    Co-authored-by: pathiknandi <pathik.nandi@tarento.com>
    Co-authored-by: Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
    Co-authored-by: Ruksana Semeir <111083125+ruksana2808@users.noreply.github.com>

---
+++ src/proxies_v8/contentTranscodeAPIIntegration.ts
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import express from 'express'
+import { axiosRequestConfig } from '../configs/request.config'
+import { CONSTANTS } from '../utils/env'
+import { logError, logInfo } from '../utils/logger'
+
+export const contentTranscodeAPIIntegration = express.Router()
+
+contentTranscodeAPIIntegration.use('/*', async (req: express.Request, res: express.Response) => {
+  try {
+    const baseUrl = removePrefix('/proxies/v8/', req.originalUrl)
+    logInfo(`The url is... ${baseUrl} : originalUrl: ${req.originalUrl}`)
+    const subPath = baseUrl.replace(/^\/+/, '')
+    const url = `${CONSTANTS.KONG_API_BASE}/${subPath}`
+    const requestBody = req.body
+
+    const axiosConfig = {
+      headers: {
+        Authorization: CONSTANTS.SB_API_KEY,
+        'Content-Type': 'application/json',
+        ...req.headers,
+      },
+      validateStatus: () => true,
+      ...axiosRequestConfig,
+    }
+
+    let response
+    if (req.method === 'GET') {
+      response = await axios.get(url, axiosConfig)
+    } else if (req.method === 'POST') {
+      response = await axios.post(url, requestBody, axiosConfig)
+    } else if (req.method === 'PUT') {
+      response = await axios.put(url, requestBody, axiosConfig)
+    } else if (req.method === 'DELETE') {
+      response = await axios.delete(url, axiosConfig)
+    } else {
+      return res.status(405).send({ error: `Method ${req.method} not supported` })
+    }
+
+    res.set(response.headers)
+    return res.status(response.status).send(response.data)
+
+  } catch (error) {
+    if (error.response) {
+      logError(`KONG error response: ${error.response.status}`, error.response.data)
+      res.set(error.response.headers)
+      return res.status(error.response.status).send(error.response.data)
+    }
+
+    logError(`Network/Unknown error in contentTranscodeAPIIntegration`, error.message)
+    return res.status(502).send({ error: 'Bad Gateway: Could not reach KONG API' })
+  }
+})
+
+function removePrefix(prefix: string, s: string): string {
+  return s.startsWith(prefix) ? s.substring(prefix.length) : s
+}
