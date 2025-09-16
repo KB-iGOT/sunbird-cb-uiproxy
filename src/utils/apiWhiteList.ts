21328c79cd05ac0a0ae9579dc8e31c20dbd57634
shankaragoudab-tarento <shankaragouda.biradar@tarento.com>
2025-09-16 12:43:12 +0530

    bug fix for the unautharisation

--- src/utils/apiWhiteList.ts
+++ src/utils/apiWhiteList.ts
@@ -376,7 +376,7 @@ export function apiWhiteListLogger() {
             return
         }
         const REQ_URL = req.path
-        if (!_.includes(REQ_URL, '/resource') && (req.session)) {
+        if (!_.includes(REQ_URL, '/resource') && !_.includes(REQ_URL, '/eclogin') && (req.session)) {
             if (!('userRoles' in req.session) || (('userRoles' in req.session) && (req.session.userRoles.length === 0))) {
                 logError('Portal_API_WHITELIST_LOGGER: User needs to authenticated themselves', '------', new Date().toString())
                 respond419(req, res)
