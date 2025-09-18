97d373b165a24f9c39896bcb6430ace584c5aab1
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-09-18 16:36:13 +0530

    Fix duplicate assignment in redirectUrl

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -115,7 +115,7 @@ userAuthKeyCloakEcApi.get('/', (req, res) => {
     if (isLocal) {
         redirectUrl = queryParam
     } else {
-        redirectUrl = redirectUrl = `${CONSTANTS.IIM_PORTAL_HOST}${CONSTANTS.EC_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
+        redirectUrl = `${CONSTANTS.IIM_PORTAL_HOST}${CONSTANTS.EC_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
     }
     logInfo('Redirecting to: ' + redirectUrl)
 
