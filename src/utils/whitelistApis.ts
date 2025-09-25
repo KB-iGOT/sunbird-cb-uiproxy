095983a11e59f91a52751d0e6721ac5299ac17a3
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-09-25 15:44:03 +0530

    4.8.29 dev v2 (#444) (#445)
    * added new proxies for superAdmin bulkupload
    
    * added bulk transfer to whiteListing
    
    * added user/v1/org-migration/sample-file for UI-proxy
    
    * Sp 29 dev (#440)
    
    * added new proxies for superAdmin bulkupload
    
    * added bulk transfer to whiteListing
    
    * added user/v1/org-migration/sample-file for UI-proxy
    
    * adde new ui proxies for userMigration bulk upload, result and progress
    
    * added proxies_v8 org-migration/bulk upload
    
    * spell check orgHierarchyFrameworkId
    
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6516,6 +6516,46 @@ export const API_LIST = {
                 ROLE.MDO_LEADER,
             ],
         },
+        '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarchyFrameworkId': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.MDO_LEADER,
+                ROLE.MDO_ADMIN,
+                ROLE.SPV_ADMIN,
+                ROLE.STATE_ADMIN,
+            ],
+        },
+        '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarchyFrameworkId': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.MDO_LEADER,
+                ROLE.MDO_ADMIN,
+                ROLE.SPV_ADMIN,
+                ROLE.STATE_ADMIN,
+            ],
+        },
+        '/proxies/v8/user/v1/org-migration/bulk-upload/progress/:orgId': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.MDO_LEADER,
+                ROLE.MDO_ADMIN,
+                ROLE.SPV_ADMIN,
+                ROLE.STATE_ADMIN,
+            ],
+        },
+        '/proxies/v8/user/v1/org-migration/bulk-upload/result/:fileName': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.MDO_LEADER,
+                ROLE.MDO_ADMIN,
+                ROLE.SPV_ADMIN,
+                ROLE.STATE_ADMIN,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -7346,5 +7386,9 @@ export const API_LIST = {
             '/proxies/v8/cbplan/v2/search',
             '/proxies/v8/cbplan/v2/archive',
             '/proxies/v8/user/v3/bulkupload',
+            '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarchyFrameworkId',
+            '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarchyFrameworkId',
+            '/proxies/v8/user/v1/org-migration/bulk-upload/progress/:orgId',
+            '/proxies/v8/user/v1/org-migration/bulk-upload/result/:fileName',
             ],
 }
