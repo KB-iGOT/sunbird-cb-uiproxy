aaf326c4b888db4cc51a18134f4a57180f95e552
abhishek-K-P <abhishek.koira@tarento.com>
2025-09-25 12:54:46 +0530

    spell check orgHierarchyFrameworkId

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6516,7 +6516,7 @@ export const API_LIST = {
                 ROLE.MDO_LEADER,
             ],
         },
-        '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarcgyFrameworkId': {
+        '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarchyFrameworkId': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
@@ -6526,7 +6526,7 @@ export const API_LIST = {
                 ROLE.STATE_ADMIN,
             ],
         },
-        '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarcgyFrameworkId': {
+        '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarchyFrameworkId': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
@@ -7386,8 +7386,8 @@ export const API_LIST = {
             '/proxies/v8/cbplan/v2/search',
             '/proxies/v8/cbplan/v2/archive',
             '/proxies/v8/user/v3/bulkupload',
-            '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarcgyFrameworkId',
-            '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarcgyFrameworkId',
+            '/proxies/v8/user/v1/org-migration/sample-file/:orgHierarchyFrameworkId',
+            '/proxies/v8/user/v1/org-migration/bulk-upload/:orgHierarchyFrameworkId',
             '/proxies/v8/user/v1/org-migration/bulk-upload/progress/:orgId',
             '/proxies/v8/user/v1/org-migration/bulk-upload/result/:fileName',
             ],
