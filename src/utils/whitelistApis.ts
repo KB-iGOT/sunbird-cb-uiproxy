0c72871c968af34692050ca88786025da6e68313
Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2025-10-10 19:22:51 +0530

    KB-11739 Adding assignment upload API  in proxyV8 (#452)
    * KB-11071 Adding assignment in proxyV8 and onboading assignment APIS
    
    * KB-11739 this is the ui-proxy config for assignment upload to GCP container
    
    * Updateing API end point for submiting answer for assignment

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6597,6 +6597,13 @@ export const API_LIST = {
             ROLE_CHECK: [
                 ROLE.PROGRAM_INSTRUCTOR,
             ],
+        },
+       '/proxies/v8/storage/v1/bp/assignment/answer/:contentId/:batchId/:formId': {
+           checksNeeded: [CHECK.ROLE],
+           // tslint:disable-next-line: object-literal-sort-keys
+           ROLE_CHECK: [
+               ROLE.PUBLIC,
+           ],
         },      
     },
     URL_PATTERN:
@@ -7438,5 +7445,6 @@ export const API_LIST = {
             '/proxies/v8/assignment/v1/submitDraft',
             '/proxies/v8/assignment/v1/submit',
             '/proxies/v8/assignment/v1/feedback',
+            '/proxies/v8/storage/v1/bp/assignment/answer/:contentId/:batchId/:formId',
             ],
 }
