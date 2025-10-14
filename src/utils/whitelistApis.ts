c03abbd858ecf4d55d82b41a3cadd5a21c8d081a
Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2025-10-14 20:41:03 +0530

    changeing pathvariable to requestparam

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6598,14 +6598,14 @@ export const API_LIST = {
                 ROLE.PROGRAM_INSTRUCTOR,
             ],
         },
-       '/proxies/v8/storage/v1/bp/assignment/answer/:contentId/:batchId/:formId': {
+        '/proxies/v8/storage/v1/bp/assignment/answer/:contentId/:batchId/:formId': {
            checksNeeded: [CHECK.ROLE],
            // tslint:disable-next-line: object-literal-sort-keys
            ROLE_CHECK: [
                ROLE.PUBLIC,
            ],
         },
-       '/proxies/v8/storage/v1/bp/assignment/answer/read/file/:contentId/:batchId/:formId/:fileName': {
+        '/proxies/v8/storage/v1/bp/assignment/answer/read/file': {
            checksNeeded: [CHECK.ROLE],
            // tslint:disable-next-line: object-literal-sort-keys
            ROLE_CHECK: [
@@ -7453,6 +7453,6 @@ export const API_LIST = {
             '/proxies/v8/assignment/v1/submit',
             '/proxies/v8/assignment/v1/feedback',
             '/proxies/v8/storage/v1/bp/assignment/answer/:contentId/:batchId/:formId',
-            '/proxies/v8/storage/v1/bp/assignment/answer/read/file/:contentId/:batchId/:formId/:fileName',
+            '/proxies/v8/storage/v1/bp/assignment/answer/read/file',
             ],
 }
