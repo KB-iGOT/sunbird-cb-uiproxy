ffe7dbe862da88073187282a3467f607b8f1ad55
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-10-24 18:14:08 +0530

    "adding logs to identify session issue"

--- src/utils/apiWhiteList.ts
+++ src/utils/apiWhiteList.ts
@@ -377,7 +377,7 @@ export function apiWhiteListLogger() {
         }
         const REQ_URL = req.path
         if (!_.includes(REQ_URL, '/resource') && !_.includes(REQ_URL, '/eclogin') && (req.session)) {
-            logInfo('Portal_API_WHITELIST_LOGGER: Incoming req.session :: ' + req.session)
+            logInfo('Portal_API_WHITELIST_LOGGER: Incoming req.session :: ' + JSON.stringify(req.session))
             if (!('userRoles' in req.session) || (('userRoles' in req.session) && (req.session.userRoles.length === 0))) {
                 logError('Portal_API_WHITELIST_LOGGER: User needs to authenticated themselves', '------', new Date().toString())
                 respond419(req, res)
