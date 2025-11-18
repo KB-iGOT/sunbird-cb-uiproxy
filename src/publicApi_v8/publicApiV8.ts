5164562ff2b285259565cb17d6193b92c2b6b400
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-11-18 17:17:23 +0530

    KB-12145 | BE |DEV | Public APIs for the Forms (#501)
    1. Increased the version since we need to add configure new API in Kong.

--- src/publicApi_v8/publicApiV8.ts
+++ src/publicApi_v8/publicApiV8.ts
@@ -104,11 +104,11 @@ publicApiV8.use('/public/assessment/v7/result', proxyCreatorRoute(express.Router
 
 publicApiV8.use('/org/v1/read', proxyCreatorRoute(express.Router(), CONSTANTS.KONG_API_BASE + '/org/v1/read'))
 
-publicApiV8.use('/forms/v2/getFormById', proxyCreatorRoute(express.Router(), API_END_POINTS.publicGetFormById))
+publicApiV8.use('/public/forms/v2/getFormById', proxyCreatorRoute(express.Router(), API_END_POINTS.publicGetFormById))
 
 publicApiV8.use('/forms/v2/getApplicationsById', proxyCreatorRoute(express.Router(), API_END_POINTS.publicGetApplicationsById))
 
-publicApiV8.use('/forms/v2/saveFormSubmit', proxyCreatorRoute(express.Router(), API_END_POINTS.publicFormSubmit))
+publicApiV8.use('/public/forms/v2/saveFormSubmit', proxyCreatorRoute(express.Router(), API_END_POINTS.publicFormSubmit))
 
 publicApiV8.get('/careers/list', async (_, res) => {
    await fetchList('Jobs', res)
