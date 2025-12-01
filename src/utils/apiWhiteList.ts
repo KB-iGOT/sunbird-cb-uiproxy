71dacf8a1a901e07e83f86c453219638299e6282
Rajeev Sathish <rajeevsathish@TI-MAC-104.local>
2025-12-01 14:26:32 +0530

    KB-0000 adding log for parichay

--- src/utils/apiWhiteList.ts
+++ src/utils/apiWhiteList.ts
@@ -377,6 +377,11 @@ export function apiWhiteListLogger() {
         }
         const REQ_URL = req.path
         if (!_.includes(REQ_URL, '/resource') && !_.includes(REQ_URL, '/eclogin') && (req.session)) {
+            if (!('userRoles' in req.session)) {
+            logInfo('checking for parichay issue need to see what all role are there in the session', req.session.userrole)
+            } else {    
+                logInfo('the userRole is not there in the session', req.session.userrole)
+            }
             if (!('userRoles' in req.session) || (('userRoles' in req.session) && (req.session.userRoles.length === 0))) {
                 logError('Portal_API_WHITELIST_LOGGER: User needs to authenticated themselves', '------', new Date().toString())
                 respond419(req, res)
