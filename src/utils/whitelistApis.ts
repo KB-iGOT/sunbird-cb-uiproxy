eb7a67a700148323400dc61e47435b411670cca7
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-12-09 14:42:58 +0530

    "KB-11881 : added a new ext patch endpoint"

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6714,6 +6714,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/user/v3/extPatch': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -7568,5 +7575,6 @@ export const API_LIST = {
             '/proxies/v8/workflow/blendedprogram/nominate',
             '/proxies/v8/promotionalcontent/v1/assignedto/users',
             '/proxies/v8/otp/v4/verify',
+            '/proxies/v8/user/v3/extPatch',
             ],
 }
