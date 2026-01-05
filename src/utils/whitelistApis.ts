3df2aa87ba96d296561de0a2b7051311c4e489db
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2026-01-05 11:08:52 +0530

    KB-11806 learning pathway enroll api (#540)
    * KB-11806 learning pathway enroll api
    
    * Update enrol API endpoint to include ID parameter

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6875,6 +6875,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/learningpathway/v1/enrol/:id': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -7749,5 +7756,6 @@ export const API_LIST = {
             '/proxies/v8/contentpartner/register/v1/update',
             '/proxies/v8/contentpartner/register/v1/read/:id',
             '/proxies/v8/contentpartner/register/v1/search',
+            '/proxies/v8/learningpathway/v1/enrol/:id',
             ],
 }
