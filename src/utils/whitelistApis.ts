9e2607da08a8d4a807e7502d00fb09a39e44f8eb
arpithasureshappa <arpitha.sureshappa@tarento.com>
2026-02-24 12:20:47 +0530

    KB-13181 create, update,publish and delete APIs added for knowledge centre

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -6991,6 +6991,34 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/knowledge/centre/create/:type': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                      ROLE.SPV_ADMIN,
+                  ],
+        },
+        '/proxies/v8/knowledge/centre/update/:type/:id': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                      ROLE.SPV_ADMIN,
+                  ],
+        },
+        '/proxies/v8/knowledge/centre/publish/:type/:id': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                      ROLE.SPV_ADMIN,
+                  ],
+        },
+        '/proxies/v8/knowledge/centre/delete/:type/:id': {
+                  checksNeeded: [CHECK.ROLE],
+                  // tslint:disable-next-line: object-literal-sort-keys
+                  ROLE_CHECK: [
+                      ROLE.SPV_ADMIN,
+                  ],
+        },
     },
     URL_PATTERN:
         [
