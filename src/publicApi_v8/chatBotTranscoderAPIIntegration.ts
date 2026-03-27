d63fbfba5bd0b9e3313285184842db3849b7da54
rajeevsathish <rajeev.sathish@tarento.com>
2026-03-27 13:29:03 +0530

    KB-0000 merging code from 34 and 35

--- src/publicApi_v8/chatBotTranscoderAPIIntegration.ts
+++ src/publicApi_v8/chatBotTranscoderAPIIntegration.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import express from 'express'
 import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
-import { logDebug, logError } from '../utils/logger'
+import { logDebug, logError, logInfo } from '../utils/logger'
 import { ERROR } from '../utils/message'
 import { extractUserTokenFromRequest } from '../utils/requestExtract'
 
