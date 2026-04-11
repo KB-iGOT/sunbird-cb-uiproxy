4c8982f7e8607f19850bcdd996077bd565d2590b
rajeevsathish <rajeev.sathish@tarento.com>
2026-04-11 16:14:57 +0530

    changes to handle the pdf and svg download

--- src/configs/framework.config.ts
+++ src/configs/framework.config.ts
@@ -43,9 +43,9 @@ module.exports = {
         },
     },
     logLevel: 'error',
-    pluginBasePath: __dirname + '/../../node_modules/',
+    pluginBasePath: __dirname + '/../node_modules/',
     plugins: [
-        { id: '@project-sunbird/form-service', ver: '1.0'},
+            { id: '@project-sunbird/form-service', ver: '1.0'},
         // { id: '@project-sunbird/review-comment', ver: '1.0' },
         // { id: '@project-sunbird/discussion-service', ver: '1.0' }
         // { id: '@project-sunbird/program', ver: '1.0' }
