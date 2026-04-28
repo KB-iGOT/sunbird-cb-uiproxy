7b1f9e423419be388a9fd5ea15be34119ed33590
vishnu <vishnubansaltarento>
2026-04-28 18:01:39 +0530

    update for ai assesment

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -120,7 +120,7 @@ userAuthKeyCloakEcApi.get('/', (req, res) => {
         
     } else {
         // tslint:disable-next-line: max-line-length
-        redirectUrl = `${CONSTANTS.IIM_PORTAL_HOST}${CONSTANTS.EC_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
+        redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
     }
     logDebug('Redirecting to: ' + redirectUrl)
 
