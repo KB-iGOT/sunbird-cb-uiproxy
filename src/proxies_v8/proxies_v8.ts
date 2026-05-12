371d52970b152a7336ac865217e512a481fb0796
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2026-05-12 17:39:32 +0530

    added content health report and summary api (#709)

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -1491,3 +1491,7 @@ proxiesV8.use('/peervalidation/*',
 proxiesV8.use('/badge/*',
   proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
 )
+
+proxiesV8.use('/contenthealth/*',
+  proxyCreatorSunbird(express.Router(), `${CONSTANTS.KONG_API_BASE}`)
+)
