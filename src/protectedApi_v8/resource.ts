6654cfcb1b3875e49babbc6c768e34ed3b73e5f6
vishnu <vishnubansaltarento>
2026-06-22 11:17:03 +0530

    update for iiidem page redirection issue

--- src/protectedApi_v8/resource.ts
+++ src/protectedApi_v8/resource.ts
@@ -4,6 +4,7 @@ import { logDebug } from '../utils/logger'
 const _                 = require('lodash')
 export const userAuthKeyCloakApi = Router()
 export const userAuthKeyCloakEcApi = Router()
+export const userAuthKeyCloakAssessmentLoginApi = Router()
 userAuthKeyCloakApi.get('/', (req, res) => {
     const host = req.get('host')
     let queryParam = ''
@@ -116,10 +117,79 @@ userAuthKeyCloakEcApi.get('/', (req, res) => {
         redirectUrl = queryParam
     } else if (queryParam && queryParam.includes('aiassessmentlogin')) {
         // tslint:disable-next-line: max-line-length
-        redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}`
+       // redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}`
 
     } else {
         // tslint:disable-next-line: max-line-length
+        //redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
+
+        redirectUrl = `${CONSTANTS.IIM_PORTAL_HOST}${CONSTANTS.EC_REDIRECT_PATH}${queryParam}`
+    }
+    logDebug('Redirecting to: ' + redirectUrl)
+
+    res.redirect(redirectUrl)
+})
+
+userAuthKeyCloakAssessmentLoginApi.get('/', (req, res) => {
+    const host = req.get('host')
+    let queryParam = ''
+    let isLocal = 0
+    let domain = ''
+    logDebug('Received query param: ' + JSON.stringify(req.query))
+    if (req.session && req.session.authenticated ) {
+        logDebug('User is authenticated.. Updating Cookie with Secure and SameSite flags')
+        if (host !== undefined) {
+            if (host.includes('localhost')) {
+                domain = 'localhost' // For localhost, set domain to localhost
+            } else {
+                const hostParts = host.split('.')
+                if (hostParts.length > 2) {
+                    domain = '.' + hostParts.slice(1).join('.')
+                } else {
+                    domain = host
+                }
+            }
+        }
+        const COOKIE_NAME = 'connect.sid'
+        const COOKIE_OPTIONS = {
+                httpOnly: true,
+                secure: true,
+            }
+        res.clearCookie(COOKIE_NAME, {
+                        COOKIE_OPTIONS,
+          })
+        res.cookie(COOKIE_NAME, req.cookies[COOKIE_NAME], { domain, maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
+          sameSite: 'None', ...COOKIE_OPTIONS })
+
+        // res.cookie('express.sid', req.cookies['express.sid'], {
+        //     httpOnly: true,
+        //     maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
+        //     sameSite: 'Lax',
+        //     secure: true,
+        // })
+    }
+    if (!_.isEmpty(req.query)) {
+        queryParam = req.query.q
+        if (queryParam && queryParam.includes('localhost')) {
+            isLocal = 1
+        }
+        if (req.query.redirect_uri) {
+            logDebug('Received redirectUrl value : ' + req.query.redirect_uri)
+            res.redirect(req.query.redirect_uri)
+            return
+        }
+    }
+    let redirectUrl = ''
+    if (isLocal) {
+        redirectUrl = queryParam
+    } else if (queryParam && queryParam.includes('aiassessmentlogin')) {
+        // tslint:disable-next-line: max-line-length
+       // redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}`
+
+    } else {
+        // tslint:disable-next-line: max-line-length
+        //redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
+
         redirectUrl = `${CONSTANTS.AI_ASSESSMENT_PORTAL_HOST}${CONSTANTS.AI_ASSESSMENT_REDIRECT_PATH}${queryParam}` //   'https://' + host + '/page/home'
     }
     logDebug('Redirecting to: ' + redirectUrl)
