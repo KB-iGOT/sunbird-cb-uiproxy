f4e82c897375cade10379a07de5e23ba3ad80c70
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2026-07-12 21:20:02 +0530

    KB-14578 fix for ngo bulk upload

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -7807,6 +7807,7 @@ export const API_LIST = {
           ROLE_CHECK: [
             ROLE.MDO_ADMIN,
             ROLE.SPV_ADMIN,
+            ROLE.MDO_LEADER,
           ],
         },
         '/proxies/v8/ratings/v1/read/:activity_id/:type': {
