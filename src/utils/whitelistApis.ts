f1139ec45295fd568a2053dd10b8f581c570154c
ruksanasemeir <ruksana.semeir@tarento.com>
2026-07-14 00:14:19 +0530

    KB-14716 | KB-14988 | Pincode and Tag values are not getting saved while creating an user individually - MDO Admin and MDO Leader

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -2584,6 +2584,7 @@ export const API_LIST = {
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
                 ROLE.PUBLIC,
+                ROLE.VOLUNTEER,
             ],
         },
         '/proxies/v8/user/v1/admin/autocomplete/:searchTerm': {
