b67bf3ffe9eaed45ae1909894bff580a6b8eaacf
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2026-08-11 10:59:27 +0530

    KB-15041 End point added for SAML deeplink for Idp(SBI) providers. (#946)
    Co-authored-by: arpithasureshappa <arpitha.sureshappa@tarento.com>
    Co-authored-by: Rajeev Sathish <rajeev.sathish@tarento.com>

--- src/publicApi_v8/publicApiV8.ts
+++ src/publicApi_v8/publicApiV8.ts
@@ -9,6 +9,7 @@ import { chatBotTranscoderAPIIntegration } from './chatBotTranscoderAPIIntegrati
 import { ntpcAuth } from './ntpcAuth'
 import { oilAuth } from './oilAuth'
 import { parichayAuth } from './parichayAuth'
+import { samlDeeplink } from './samlDeeplink'
 import { workallocationPublic } from './workallocationPublic'
 import { youtubePlaylist } from './youtubePlaylist'
 
@@ -90,6 +91,8 @@ publicApiV8.use('/parichay', parichayAuth)
 publicApiV8.use('/oil', oilAuth)
 
 publicApiV8.use('/ntpc', ntpcAuth)
+
+publicApiV8.use('/saml', samlDeeplink)
 
 publicApiV8.use('/halloffame/read', proxyCreatorRoute(express.Router(), CONSTANTS.KONG_API_BASE + '/halloffame/read'))
 
