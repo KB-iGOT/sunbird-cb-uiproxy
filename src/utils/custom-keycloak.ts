5a1331a51457a426b09f40af7da4413bad4029cf
Abhishek K P <abhishek.koira@tarentoc.om>
2026-09-03 18:19:40 +0530

    reverted to 40.1-KB

--- src/utils/custom-keycloak.ts
+++ src/utils/custom-keycloak.ts
@@ -140,9 +140,6 @@ export async function deauthenticateKeycloakSession(reqObj: any): Promise<void> 
 }
 
 export class CustomKeycloak {
-
-  // tslint:disable-next-line: no-any
-  deauthenticated = deauthenticateKeycloakSession
   private multiTenantKeycloak = new Map<string, InstanceType<typeof keycloakConnect>>()
 
   constructor(sessionConfig: expressSession.SessionOptions) {
@@ -470,6 +467,9 @@ export class CustomKeycloak {
     }
     logDebug(`${process.pid}: User Deauthenticated New`)
   }
+
+  // tslint:disable-next-line: no-any
+  deauthenticated = deauthenticateKeycloakSession
 
   protect = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const keycloak = this.getKeyCloakObject(req)
