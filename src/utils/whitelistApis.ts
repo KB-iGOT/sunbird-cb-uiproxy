2c4efe55d9ea04901a78d939f2b91d98f2859f05
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2026-09-19 21:34:52 +0530

    KB-15583 | DEV|BE| Implementation of the User Content Info V2  API
    Proxy added for user content info v2

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -8277,6 +8277,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/user/v2/content/info': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+                ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -9318,5 +9325,6 @@ export const API_LIST = {
             '/proxies/v8/action/ca/v1/publish/:id',
             '/proxies/v8/ca/questionset/v1/publish/:id',
             '/proxies/v8/cbplan/v4/user/dictionary',
+            '/proxies/v8/user/v2/content/info',
         ],
 }
