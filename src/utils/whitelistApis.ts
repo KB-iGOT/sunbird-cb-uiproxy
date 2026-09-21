12f3775ae7fd265f21ccc785e0f6de04f8ee19b4
ramyaranganathan-1775 <ramya.ranganathan@tarento.com>
2026-09-21 09:19:38 +0530

    KB-15587: CA enrollment

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -8277,6 +8277,14 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/ca/v1/autoenrollment/:caId': {
+          checksNeeded: [CHECK.ROLE],
+          // tslint:disable-next-line: object-literal-sort-keys
+          ROLE_CHECK: [
+            ROLE.PUBLIC,
+            ROLE.VOLUNTEER,
+          ],
+        },
     },
     URL_PATTERN:
         [
@@ -9318,5 +9326,6 @@ export const API_LIST = {
             '/proxies/v8/action/ca/v1/publish/:id',
             '/proxies/v8/ca/questionset/v1/publish/:id',
             '/proxies/v8/cbplan/v4/user/dictionary',
+            '/proxies/v8/ca/v1/autoenrollment',
         ],
 }
